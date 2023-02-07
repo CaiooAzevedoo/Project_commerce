ec54f99ba58530947677facb608550b1af116038
Caio <caiusito@gmail.com>
2023-02-07 16:26:01 -0300

    fix Lint

--- challenges/desafio6.js
+++ challenges/desafio6.js
@@ -1,3 +1,3 @@
-db.produtos.find({ $and: [{ curtidas: { $gt: 10 }}, 
-  { curtidas: { $lt: 100 }}] },
-  { nome: 1, _id: false, curtidas: 1});
+db.produtos.find({ $and: [{ curtidas: { $gt: 10 } }, 
+  { curtidas: { $lt: 100 } }] },
+  { nome: 1, _id: false, curtidas: 1 });
