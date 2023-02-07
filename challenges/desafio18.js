677b922312b25fea4cddeb1081e485b95e30ff0a
Caio <caiusito@gmail.com>
2023-02-07 17:18:26 -0300

    fix Requisito 18

--- challenges/desafio18.js
+++ challenges/desafio18.js
@@ -1,3 +1,3 @@
 db.produtos.updateMany({ nome: { $in: ["Big Mac", "Quarteirão com Queijo"] } },
 { $push: { ingredientes: "bacon" } });
-db.produtos.find({}, { name: 1, ingredientes: 1, _id: 0 });
+db.produtos.find({}, { nome: 1, ingredientes: 1, _id: 0 });
